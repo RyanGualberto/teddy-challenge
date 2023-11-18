5ab4ef59a7f1e099eb4d8e7700cc5f7f8a176d07
ryangualberto <ryanoliveiragualberto@gmail.com>
2023-11-18 08:53:11 -0300

    fix: loggin database connection

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const databaseUrl = process.env.DATABASE_URL;
-
+console.log("Connecting to database... ", databaseUrl);
 mongoose.connect(databaseUrl);
 
 module.exports = mongoose;
